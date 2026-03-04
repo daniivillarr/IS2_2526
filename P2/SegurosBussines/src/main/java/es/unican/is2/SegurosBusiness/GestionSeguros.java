c92d8a3249c131a32d911f46d852d34ae841434d
daniivillarr <danivillarcorro@gmail.com>
2026-03-04 13:22:44 +0100

    Correcion algunos errores

---
+++ P2/SegurosBussines/src/main/java/es/unican/is2/SegurosBusiness/GestionSeguros.java
@@ -0,0 +1,49 @@
+package es.unican.is2.SegurosBusiness;
+
+import es.unican.is2.SegurosCommon.*;
+
+public class GestionSeguros implements IGestionClientes, IGestionSeguros, IInfoSeguros {
+
+	@Override
+	public Cliente cliente(String dni) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Seguro seguro(String matricula) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Seguro nuevoSeguro(Seguro s, String dni) throws OperacionNoValida, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Seguro bajaSeguro(String matricula, String dni) throws OperacionNoValida, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Seguro anhadeConductorAdicional(String matricula, String conductor) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente nuevoCliente(Cliente c) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente bajaCliente(String dni) throws OperacionNoValida, DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
